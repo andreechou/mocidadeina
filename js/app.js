8c9e6cc0e7df7598546f0a6ef53dafdaadba0d62
Andre Chou <andreechou@gmail.com>
2016-09-11 15:45:19 -0300

    v2

--- js/app.js
+++ js/app.js
@@ -10,56 +10,139 @@ $.when(
   })
 ).then(function() {
 
-  // Console
-  // console.log('---events');
-  // console.log(events)
-
-  // console.log('---information')
-  // console.log(informations)
-
-  // console.log('---getting')
-  // console.log(events.feed.entry)
-  // console.log(events.feed.entry[2].content)
-
-
   // Data Organizer
   
+  eventsNum = 7;
   events = []
   for (var i = 7; i <= eventsTemp.length - 1; i = i + 7) {
-    events[(i-7)/7] = {}
-    events[(i-7)/7][eventsTemp[0].content.$t] = eventsTemp[i].content.$t
-    events[(i-7)/7][eventsTemp[1].content.$t] = eventsTemp[i+1].content.$t
-    events[(i-7)/7][eventsTemp[2].content.$t] = eventsTemp[i+2].content.$t
-    events[(i-7)/7][eventsTemp[3].content.$t] = eventsTemp[i+3].content.$t
-    events[(i-7)/7][eventsTemp[4].content.$t] = eventsTemp[i+4].content.$t
-    events[(i-7)/7][eventsTemp[5].content.$t] = eventsTemp[i+5].content.$t
-    events[(i-7)/7][eventsTemp[6].content.$t] = eventsTemp[i+6].content.$t
+    events[(i-eventsNum)/eventsNum] = {}
+    for(var j = 0; j <= eventsNum - 1; j++) {
+      if(eventsTemp[i+j].content.$t == '[empty]') {
+        events[(i-eventsNum)/eventsNum][eventsTemp[j].content.$t] = ''
+      } else {
+        events[(i-eventsNum)/eventsNum][eventsTemp[j].content.$t] = eventsTemp[i+j].content.$t
+      }
+    }
   }
   console.log(events)
-
+  infoNum = 6
   informations = []
-  for (var i = 4; i <= informationsTemp.length - 1; i = i + 4) {
-    informations[(i-4)/4] = {}
-    informations[(i-4)/4][informationsTemp[0].content.$t] = informationsTemp[i].content.$t
-    informations[(i-4)/4][informationsTemp[1].content.$t] = informationsTemp[i+1].content.$t
-    informations[(i-4)/4][informationsTemp[2].content.$t] = informationsTemp[i+2].content.$t
-    informations[(i-4)/4][informationsTemp[3].content.$t] = informationsTemp[i+3].content.$t
+  for (var i = infoNum; i <= informationsTemp.length - 1; i = i + infoNum) {
+    informations[(i-infoNum)/infoNum] = {}
+    for(var j = 0; j <= infoNum - 1; j++) {
+      if(informationsTemp[i+j].content.$t == '[empty]') {
+        informations[(i-infoNum)/infoNum][informationsTemp[j].content.$t] = ''
+      } else {
+        informations[(i-infoNum)/infoNum][informationsTemp[j].content.$t] = informationsTemp[i+j].content.$t
+      }
+    }
   }
   console.log(informations)
 
-
-  // Filters
-  Vue.filter('content', function (value) {
-    return marked(value.slice(5))
-  })
-
-
   // Vue
-  new Vue({
-    el: '#app',
-    data: {
-      events: events,
-      informations: informations
+
+  var App = Vue.extend({})
+
+  // Pages
+
+  var pgEventos = Vue.extend({
+    template: '#pgEventos',
+    data: function() {
+      return {
+        events: events
+      }
     }
   })
+
+  var pgEvento = Vue.extend({
+    template: '#pgEvento',
+    data: function() {
+      return {
+        events: events
+      }
+    }
+  })
+
+  var pgInformacoes = Vue.extend({
+    template: '#pgInformacoes',
+    data: function() {
+      return {
+        informations: informations
+      }
+    }
+  })
+
+  var pgInformacao = Vue.extend({
+    template: '#pgInformacao',
+    data: function() {
+      return {
+        informations: informations
+      }
+    }
+  })
+
+  var pgPrint = Vue.extend({
+    template: '#pgPrint'
+  })
+
+  var pgPrintMonth = Vue.extend({
+    template: '#pgPrintMonth',
+    data: function() {
+      return {
+        events: events
+      }
+    }
+  })
+
+  var pgPrintDay = Vue.extend({
+    template: '#pgPrintDay',
+    data: function() {
+      return {
+        events: events
+      }
+    }
+  })
+
+  // Filters
+
+  Vue.filter('content', function (value) {
+    return marked(value)
+  })
+
+  Vue.filter('map', function (value) {
+    return value.replace('width="600" height="450"', 'width="100%" height="240"')
+  })
+
+  // Router
+
+  var router = new VueRouter()
+
+  router.map({
+    '/': {
+      component: pgEventos
+    },
+    '/event/:id': {
+      component: pgEvento,
+      name: 'event'
+    },
+    '/infos': {
+      component: pgInformacoes
+    },
+    '/info/:id': {
+      component: pgInformacao,
+      name: 'information'
+    },
+    '/print': {
+      component: pgPrint
+    },
+    '/month': {
+      component: pgPrintMonth
+    },
+    '/day': {
+      component: pgPrintDay
+    }
+  })
+
+  router.start(App, '#app')
+
 });
---
+++ readme.md
@@ -0,0 +1,18 @@
+# Objetivo
+
+Criar um site que qualquer um possa atualizá-lo usando Google Spreadsheet para divulgar o calendário e os eventos dos jovens da Igreja Nova Aliança.
+
+# Conteúdo
+
+- Calendário
+- Detalhe dos eventos
+    + Data
+    + Local (Endereço e Google Maps)
+    + Descrição
+- Print do mês (Calendário do mês para usar no grupo do Whats)
+- Informações Fixas (Endereço, horário, telefone e descrição)
+    + Igreja
+    + Quadra
+    + Capelania
+- Foto (Instagram usando #mocidadeina)
+
